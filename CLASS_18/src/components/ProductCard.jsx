f8686fe1b74c71b292717ecf4627f337068a7d55
Sk Nayeem <sknayeemislam384@gmail.com>
2025-07-21 01:23:18 +0600

    Build a Shopping Cart app using Zustand And React query

--- CLASS_18/src/components/ProductCard.jsx
+++ CLASS_18/src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
+import { useCartStore } from "../zustand/store/store";
+
 const ProductCard = ({ product }) => {
+  const dispatch = useCartStore(state => state.dispatch)
+
   return (
     <div className="border rounded-2xl p-4 shadow-lg hover:shadow-xl transition-shadow duration-300 bg-white flex flex-col justify-between">
       <div>
+       <div className="relative h-48 rounded-xl overflow-hidden mb-4">
         <img src={product.image}alt={product.title}
-          className="h-40 w-[100%] object-cover rounded-xl mb-4"/>
+          className="h-full w-full object-cover group-hover:scale-105 transition-transform duration-300"/>
+        <span className="absolute top-2 right-2 bg-indigo-600 text-white text-xs px-3 py-1 rounded-full shadow">{product.category}</span>
+      </div>
         <div className="space-y-1">
           <p className="text-xs text-gray-400">ID: {product.id}</p>
           <h2 className="text-xl font-bold text-gray-800">{product.title}</h2>
@@ -12,7 +19,8 @@ const ProductCard = ({ product }) => {
           <p className="text-sm italic text-gray-500">Category: {product.category}</p>
         </div>
       </div>
-      <button className="bg-indigo-500 text-white w-full py-2 mt-5 rounded-lg font-semibold hover:bg-indigo-600 transition duration-200">
+      <button onClick={() => dispatch({type: "ADD_TO_CART", payload: product})}
+       className="bg-indigo-500 text-white w-full py-2 mt-5 rounded-lg font-semibold hover:bg-indigo-600 transition duration-200">
         Add To Cart
       </button>
     </div>
--- CLASS_18/src/components/ProductList.jsx
+++ CLASS_18/src/components/ProductList.jsx
@@ -5,8 +5,7 @@ import { products } from '../enum/enum'
 const ProductList = () => {
   return (
     <Fragment>
-        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">Our Products</h1>
-      <div className="w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+      <div className="w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (<ProductCard key={product.id} product={product}/>))}
       </div>
     </Fragment>
