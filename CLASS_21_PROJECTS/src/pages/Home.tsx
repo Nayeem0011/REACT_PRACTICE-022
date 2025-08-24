cff42835a24760c42181daa5ff7e5d8f76fc2e52
Sk Nayeem <sknayeemislam384@gmail.com>
2025-08-24 13:02:37 +0600

    Build a Recipe Finder app with API calls and React components

--- CLASS_21_PROJECTS/src/pages/Home.tsx
+++ CLASS_21_PROJECTS/src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react"
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [searchTearm, setSearchTearm] = useState("")
+
+  useEffect(()=>{
+    const fetchResponse = async() =>{
+      setLoading(true)
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTearm}`)
+      const data = await res.json()
+      console.log("Recipes", data)
+      setLoading(false)
+    }
+    fetchResponse()
+  },[searchTearm])
+
   return (
     <div>
       
@@ -8,3 +24,4 @@ const Home = () => {
 }
 
 export default Home
+
